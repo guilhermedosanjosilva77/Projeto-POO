d2b782b296ffc5888781f3f6b6c3af338a81be00
Guilherme <dosanjosguilherme88@gmail.com>
2025-09-25 21:20:00 -0300

    NEW

---
+++ Veiculo.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Veiculo {
+    String modelo;
+    String marca;
+    double velocidadeAtual;
+    
+
+
+    public Veiculo(String modelo, String marca, double velocidadeAtual) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.velocidadeAtual = velocidadeAtual;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public double getVelocidadeAtual() {
+        return velocidadeAtual;
+    }
+    public void setVelocidadeAtual(double velocidadeAtual) {
+        this.velocidadeAtual = velocidadeAtual;
+    }
+
+    public String gerarRelatorio(){
+        return 
+    }
+    public String monitorarDesempenho(){
+        return
+
+    }
+
+    
+
+
+}
